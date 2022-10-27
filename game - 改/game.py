4467d16aadea34b06dc1a49438a856ea5c5bfe95
weiwei0703 <114126738+weiwei0703@users.noreply.github.com>
2022-10-27 10:39:37 +0800

    Add files via upload

--- game - 改/game.py
+++ game - 改/game.py
@@ -22,6 +22,7 @@ background_img = pygame.image.load(os.path.join("img","background.png")).convert
 player_img = pygame.image.load(os.path.join("img","player.png")).convert()
 player_mini_img = pygame.transform.scale(player_img,(25,19))
 player_mini_img.set_colorkey(BLACK)
+pygame.display.set_icon(player_mini_img)
 # rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()
 bullet_img = pygame.image.load(os.path.join("img","bullet.png")).convert()
 rock_imgs = []
@@ -99,8 +100,10 @@ def draw_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                return True
             elif event.type == pygame.KEYUP:
                 waiting = False
+                return False
   
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -271,7 +274,9 @@ running = True
 #game
 while running:
     if show_init:
-        draw_init()
+        close=draw_init()
+        if close:
+            break
         show_init = False
         all_sprites = pygame.sprite.Group()
         rocks = pygame.sprite.Group()
